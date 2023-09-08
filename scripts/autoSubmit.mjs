951177cf87a7736220da78a7e8308f68f0336110
CanisMinor <i@canisminor.cc>
2023-09-08 17:53:10 +0800

    🛠️ chore: merge [skip ci]

--- scripts/autoSubmit.mjs
+++ scripts/autoSubmit.mjs
@@ -1,17 +1,15 @@
-import { Octokit } from '@octokit/rest';
-import 'dotenv/config';
-import { camelCase } from 'lodash-es';
-import { execSync } from 'node:child_process';
-import { existsSync, writeFileSync } from 'node:fs';
-import { resolve } from 'path';
-
-import pkg from '../package.json';
-import { formatAgentJSON } from './check.mjs';
-import { agentsDir } from './const.mjs';
-
+import { Octokit } from "@octokit/rest";
+import "dotenv/config";
+import { formatAgentJSON } from "./check.mjs";
+import { camelCase } from "lodash-es";
+import { existsSync, writeFileSync } from "node:fs";
+import { resolve } from "path";
+import { agentsDir } from "./const.mjs";
+import pkg from "../package.json";
+import { execSync } from "node:child_process";
 class AutoSubmit {
-  owner = 'lobehub';
-  repo = 'lobe-chat-agents';
+  owner = "lobehub";
+  repo = "lobe-chat-agents";
   issueNumber = process.env.ISSUE_NUMBER;
 
   constructor() {
@@ -24,7 +22,7 @@ class AutoSubmit {
     const comment = this.genCommentMessage(json);
     const agent = await this.formatIssue(issue);
     const agentName = camelCase(agent.meta.title);
-    const fileName = agentName + '.json';
+    const fileName = agentName + ".json";
     const filePath = resolve(agentsDir, fileName);
 
     // check same name
@@ -32,21 +30,21 @@ class AutoSubmit {
       await this.createComment(
         [
           `🚨 Same name exist <${`${pkg.homepage}/blob/main/agents/${fileName}`}>, please rename your agent and submit again`,
-          '---',
+          "---",
           comment,
-        ].join('\n'),
+        ].join("\n"),
       );
-      await this.addLabels('🚨 Auto Check Fail');
+      await this.addLabels('🚨 Auto Check Fail')
       return;
     }
 
     // comment in issues
     await this.createComment(comment);
-    await this.addLabels('✅ Auto Check Pass');
+    await this.addLabels('✅ Auto Check Pass')
 
     // generate file
     writeFileSync(filePath, JSON.stringify(agent, null, 2), {
-      encoding: 'utf8',
+      encoding: "utf8",
     });
 
     // commit and pull request
@@ -56,78 +54,78 @@ class AutoSubmit {
 
   gitCommit(agentName) {
     execSync(`git checkout -b agent/${agentName}`);
-    execSync('git add .');
+    execSync("git add .");
     execSync(`git commit -m "✨ feat(agent): Add ${agentName}"`);
     execSync(`git push origin agent/${agentName}`);
   }
 
   genCommentMessage(json) {
     return [
-      '🤖 Automatic generated agent config file',
-      '```json',
+      "🤖 Automatic generated agent config file",
+      "```json",
       JSON.stringify(json, null, 2),
-      '```',
-    ].join('\n');
+      "```",
+    ].join("\n");
   }
 
   async createPullRequest(agentName, body) {
     const { owner, repo, octokit, issueNumber } = this;
     const pr = await octokit.pulls.create({
-      owner: owner,
-      repo: repo,
-      title: `[AgentSubmit] ${agentName} (#${issueNumber})`,
-      head: `agent/${agentName}`,
-      base: 'main',
-      body,
-    });
+                                            owner: owner,
+                                            repo: repo,
+                                            title: `[AgentSubmit] ${agentName} (#${issueNumber})`,
+                                            head: `agent/${agentName}`,
+                                            base: "main",
+                                            body,
+                                          });
   }
   async getIssue() {
     const { owner, repo, octokit, issueNumber } = this;
     const issue = await octokit.issues.get({
-      owner,
-      repo,
-      issue_number: issueNumber,
-    });
+                                             owner,
+                                             repo,
+                                             issue_number: issueNumber,
+                                           });
     return issue.data;
   }
 
   async addLabels(label) {
     const { owner, repo, octokit, issueNumber } = this;
     await octokit.issues.addLabels({
-      owner,
-      repo,
-      issue_number: issueNumber,
-      labels: [label],
-    });
+                                     owner,
+                                     repo,
+                                     issue_number: issueNumber,
+                                     labels: [label],
+                                   });
   }
 
   async createComment(body) {
     const { owner, repo, octokit, issueNumber } = this;
     const { data } = await octokit.issues.createComment({
-      owner,
-      repo,
-      issue_number: issueNumber,
-      body,
-    });
+                                                          owner,
+                                                          repo,
+                                                          issue_number: issueNumber,
+                                                          body,
+                                                        });
     return data.id;
   }
 
   markdownToJson(markdown) {
-    const lines = markdown.split('\n');
+    const lines = markdown.split("\n");
     const json = {};
 
-    let currentKey = '';
-    let currentValue = '';
+    let currentKey = "";
+    let currentValue = "";
 
     lines.forEach((line) => {
-      if (line.startsWith('###')) {
+      if (line.startsWith("###")) {
         if (currentKey && currentValue) {
           json[currentKey] = currentValue.trim();
-          currentValue = '';
+          currentValue = "";
         }
-        currentKey = line.replace('###', '').trim();
+        currentKey = line.replace("###", "").trim();
       } else {
-        currentValue += line + '\n';
+        currentValue += line + "\n";
       }
     });
 
@@ -135,7 +133,10 @@ class AutoSubmit {
       json[currentKey] = currentValue.trim();
     }
 
-    json.tags = json.tags.replaceAll('，', ',').replaceAll(', ', ',').split(',');
+    json.tags = json.tags
+                    .replaceAll("，", ",")
+                    .replaceAll(", ", ",")
+                    .split(",");
 
     return json;
   }
@@ -148,7 +149,7 @@ class AutoSubmit {
         systemRole: json.systemRole,
       },
       homepage: data.user.html_url,
-      identifier: 'plugin identifier',
+      identifier: "plugin identifier",
       locale: json.locale,
       meta: {
         avatar: json.avatar,
