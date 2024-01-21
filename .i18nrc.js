53664a3a3feaf76a6e61244b262a389e26503cbd
arvinxx <arvinx@foxmail.com>
2024-01-21 15:54:47 +0800

    :sparkles: feat: support more locales

--- .i18nrc.js
+++ .i18nrc.js
@@ -1,6 +1,22 @@
 module.exports = {
   selectors: ['meta.title', 'meta.description', 'meta.tags', 'config.systemRole'],
   entryLocale: 'en-US',
-  outputLocales: ['zh-CN', 'en-US'],
+  outputLocales: [
+    'ar',
+    'zh-TW',
+    'en-US',
+    'ru-RU',
+    'ja-JP',
+    'ko-KR',
+    'fr-FR',
+    'tr-TR',
+    'es-ES',
+    'pt-BR',
+    'de-DE',
+    'it-IT',
+    'nl-NL',
+    'pl-PL',
+    'vi-VN',
+  ],
   modelName: 'gpt-3.5-turbo-1106',
 };
