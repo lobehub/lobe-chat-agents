945d18e0b2818590020151dde92c495f7a0ab787
canisminor1990 <i@canisminor.cc>
2024-10-08 15:17:27 +0800

    🐛 fix: Fix scripts

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -5,6 +5,22 @@ import { join } from 'node:path';
 
 import { config, readmeSplit } from './const';
 
+export const normalizeLocale = (locale?: string): string => {
+  if (!locale) return 'en-US';
+
+  if (locale.startsWith('ar')) return 'ar';
+
+  if (locale.startsWith('cn')) return 'zh-CN';
+
+  for (const l of config.outputLocales) {
+    if (l.startsWith(locale)) {
+      return l;
+    }
+  }
+
+  return 'en-US';
+};
+
 export const writeJSON = (filePath, data, format = true) => {
   const jsonStr = format ? JSON.stringify(data, null, 2) : JSON.stringify(data);
   writeFileSync(filePath, jsonStr, 'utf8');
@@ -22,12 +38,14 @@ export const split = (name) => {
 };
 
 export const getLocaleAgentFileName = (id: string, locale?: string) => {
-  const localeSuffix = locale === config.entryLocale ? '' : `.${locale}`;
+  const formatedLocale = normalizeLocale(locale);
+  const localeSuffix = formatedLocale === config.entryLocale ? '' : `.${formatedLocale}`;
   return join(id, 'index' + localeSuffix + '.json');
 };
 
 export const getBuildLocaleAgentFileName = (id: string, locale?: string) => {
-  const localeSuffix = locale === config.entryLocale ? '' : `.${locale}`;
+  const formatedLocale = normalizeLocale(locale);
+  const localeSuffix = formatedLocale === config.entryLocale ? '' : `.${formatedLocale}`;
   return id + localeSuffix + '.json';
 };
 
